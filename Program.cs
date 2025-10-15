331192c7bfa496716136e0e1933669bb169873b5
Elton Carlos do Nascimento <eltnas@outlook.com>
2025-10-15 07:40:46 -0300

    Resolução do desafio de codigo!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Objeto Nokia
+Console.Clear();
+Console.WriteLine("\n##########  Nokia  ##########\n");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+// Objeto Iphone
+Console.WriteLine("\n##########  Iphone  ##########\n");
+Smartphone iphone = new Iphone("987654321", "Iphone 13", "987654321098765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
